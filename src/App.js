f363626cdba960b26bb04ae4ad93db615e562268
Alyssa Lundgren <lundgrea@gmail.com>
2019-10-07 10:29:38 -0600

    Add fetch bones to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      hikes: [],
+      error: ''
+    }
+  }
+  componentDidMount = () => {
+    return fetch('http://localhost:3001/api/v1/hikes')
+    .then(res => console.log(res))
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
 
 export default App;
